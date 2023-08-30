067d9396e7142853d0e4087ce4c615554c34c751
Emma A. A. Rünzel <emma@ruenzel.de>
2023-08-31 00:14:16 +0200

    Add Alert Controller & Home Page Button

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -1,13 +1,31 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import Swal from 'sweetalert2';
 
-// Connects to data-controller="alert"
+// Connects to data-controller=“alert”
 export default class extends Controller {
   static values = {
-    url: String
+    html: String,
+    title: String,
+    confirmButtonText: String,
+    showCancelButton: Boolean,
+    cancelButtonText: String
   }
-
   connect() {
     console.log("Shalom 🥸")
-    // fetch(this.urlValue).then("Hello again 👋")
+  }
+  initSweetalert(event) {
+    event.preventDefault();
+    Swal.fire({
+      html: this.htmlValue,
+      title: this.titleValue,
+      showCancelButton: this.showCancelButtonValue,
+      confirmButtonText: this.confirmButtonTextValue,
+      cancelButtonText: this.cancelButtonTextValue,
+      reverseButtons: true
+    }).then((action) => {
+      if (action.isConfirmed) {
+        this.element.submit();
+      }
+    })
   }
 }
