01c25155c94f75b986466357e5da7e1b2ce8680c
Emma A. A. Rünzel <emma@ruenzel.de>
2023-09-03 15:16:26 +0200

    Clean Up Sweet Alert Controller, Add Comments & Remove Callback

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -6,31 +6,33 @@ export default class extends Controller {
   static values = {
     html: String,
     title: String,
+    icon: String,
     confirmButtonText: String,
     showCancelButton: Boolean,
     cancelButtonText: String
   }
-  connect() {
-    console.log("Shalom 🥸")
-  }
 
-  initSweetalert() {
-    Swal.fire('Hello!', 'This is a SweetAlert2 alert!', 'success');
-  }
-
-
-  // initSweetalert(event) {
-    // event.preventDefault();
-  //   Swal.fire({
-  //     html: this.htmlValue,
-  //     title: this.titleValue,
-  //     showCancelButton: this.showCancelButtonValue,
-  //     confirmButtonText: this.confirmButtonTextValue,
-  //     cancelButtonText: this.cancelButtonTextValue
-  //   }).then((action) => {
-  //     if (action.isConfirmed) {
-  //       this.element.click();
-  //     }
-  //   })
+  // initSweetalert() {
+  //   Swal.fire('Hello!', 'This is a SweetAlert2 alert!', 'success');
   // }
+
+  initSweetalert(event) {
+    event.preventDefault();
+    Swal.fire({
+      html: this.htmlValue,
+      title: this.titleValue,
+      icon: this.iconValue,
+      showCancelButton: this.showCancelButtonValue,
+      confirmButtonText: this.confirmButtonTextValue,
+      cancelButtonText: this.cancelButtonTextValue
+    })
+    // If you want to do something after
+    // the action is confirmed 👇🏻
+
+    // .then((action) => {
+    //   if (action.isConfirmed) {
+    //     this.element.click();
+    //   }
+    // })
+  }
 }
